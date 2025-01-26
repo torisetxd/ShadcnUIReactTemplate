c66eccf7c4263ab59505542ca0a5fc22c45f5288
toriset <xdragongamer14@gmail.com>
2025-01-26 22:46:59 +0200

    Got rid of all the clutter and made a simple dark theme page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-screen w-screen dark bg-background flex flex-col justify-center items-center'>
+      <h1 className='text-3xl font-bold text-white'>Your number is {count}</h1>
+      <br></br>
+      <div className='flex space-x-4'>
+        <Button onClick={() => setCount((count) => count + 1)}>Increment</Button>
+        <Button onClick={() => setCount((count) => count - 1)}>Decrement</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <p className='mt-4 text-sm text-gray-200' >Made with ❤️ by <a href="https://github.com/torisetxd">toriset</a></p>
+    </div>
   )
 }
 
