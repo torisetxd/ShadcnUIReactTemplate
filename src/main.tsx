660ea8d2855f022a807007b92609dd55c147e5bd
toriset <xdragongamer14@gmail.com>
2025-02-21 08:26:31 +0200

    Fixed themes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from 'next-themes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        <App />
+    </ThemeProvider>
   </StrictMode>,
 )
